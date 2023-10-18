a63e1d1217c1fd844e0087a6249f0eb6a4740759
awesomepras <prasana@alienfrompondy.com>
2023-10-17 22:58:51 -0400

    added cleanup script

---
+++ cleanup-kind.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+KIND_CLUSTER_NAME=$(kind get clusters)
+set -o errexit
+kind_version=$(kind version)
+kind_network='kind'
+reg_name='kind-registry'
+reg_port='5000'
+# create registry container unless it already exists
+running="$(docker inspect -f '{{.State.Running}}' "${reg_name}" 2>/dev/null || true)"
+if [ "${running}" == 'true' ]; then
+  cid="$(docker inspect -f '{{.ID}}' "${reg_name}")"
+  echo "> Stopping and deleting Kind Registry container..."
+  docker stop $cid >/dev/null
+  docker rm $cid >/dev/null
+fi
+
+echo "> Deleting Kind cluster..."
+kind delete cluster --name=$KIND_CLUSTER_NAME
