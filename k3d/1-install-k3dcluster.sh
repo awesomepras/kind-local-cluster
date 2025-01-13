e9fb51a40bf76bc991b705286cc5e812ee59076e
awesomepras <prasana@alienfrompondy.com>
2025-01-13 16:23:07 -0500

    updated layer2-config

--- k3d/1-install-k3dcluster.sh
+++ k3d/1-install-k3dcluster.sh
@@ -4,7 +4,6 @@
 # k3d does not allow direct communication to underlying host, so idea is to create a separate docker network to be used by k3d cluster
 # --------------------------------------------------------#
 create_cluster() {
-    read -p "Enter name for cluster (default: dev-cluster): " CLUSTERNAME
     # Check if CLUSTERNAME is empty and set default value
     if [ -z "${CLUSTERNAME}" ]; then
         CLUSTERNAME="dev-cluster"
@@ -17,6 +16,9 @@ DSUBNET=192.168.3.0/24
 DGWY=192.168.3.1
 
 # Read custom network name for Docker
+
+read -p "Enter name for cluster (default: dev-cluster): " CLUSTERNAME
+
 echo "Checking if docker network ${CLUSTERNAME} exists..."
 
 # Check if the Docker network exists
@@ -28,12 +30,13 @@ else
     docker network create --driver bridge --subnet=${DSUBNET} --gateway=${DGWY}  "${CLUSTERNAME}"
 fi
 # create ip masquerade 
+# sudo iptables -t nat -A POSTROUTING -s 192.168.3.0/24 ! -d 192.168.3.0/24 -j MASQUERADE
 # Check if the MASQUERADE rule exists
-if sudo iptables -t nat -C POSTROUTING -s $SUBNET ! -d $SUBNET -j MASQUERADE 2>/dev/null; then
-    echo "MASQUERADE rule for $SUBNET already exists."
+if sudo iptables -t nat -C POSTROUTING -s $DSUBNET ! -d $DSUBNET -j MASQUERADE 2>/dev/null; then
+    echo "MASQUERADE rule for $DSUBNET already exists."
 else
-    echo "MASQUERADE rule for $SUBNET does not exist. Adding the rule..."
-    sudo iptables -t nat -A POSTROUTING -s $SUBNET ! -d $SUBNET -j MASQUERADE
+    echo "MASQUERADE rule for $DSUBNET does not exist. Adding the rule..."
+    sudo iptables -t nat -A POSTROUTING -s $DSUBNET ! -d $DSUBNET -j MASQUERADE
     echo "Rule added successfully."
 fi
 
